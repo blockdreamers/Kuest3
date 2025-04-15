451de1e8a45770ebad539b90e9959876d1a8f4db
blockdreamers <blockdreamers@gmail.com>
2025-04-15 17:09:47 +0900

    privy wallet integration for myprofile

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,19 @@ import './index.css';
 import { AuthProvider } from './contexts/AuthContext';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { PrivyProvider } from '@privy-io/react-auth';
+
+const PRIVY_APP_ID = 'clw5p039n0b4lhw5umwm163ua';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <Router>
-        <App />
-        <Toaster position="top-right" />
-      </Router>
-    </AuthProvider>
+    <PrivyProvider appId={PRIVY_APP_ID} config={{ appearance: { theme: 'light' } }}>
+      <AuthProvider>
+        <Router>
+          <App />
+          <Toaster position="top-right" />
+        </Router>
+      </AuthProvider>
+    </PrivyProvider>
   </StrictMode>
 );
