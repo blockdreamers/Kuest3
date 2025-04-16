0415402904f8461bbf22a795692aa1859c5880b5
blockdreamers <blockdreamers@gmail.com>
2025-04-16 16:31:00 +0900

    superbase gogo

--- src/lib/superbase.ts
+++ src/lib/superbase.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@superbase/superbase-js';
+import { createClient } from '@supabase/supabase-js';
 
 const superbaseUrl = import.meta.env.VITE_SUPERBASE_URL!;
 const superbaseAnonKey = import.meta.env.VITE_SUPERBASE_ANON_KEY!;
