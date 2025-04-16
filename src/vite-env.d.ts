98c58e0aa89c833dc78a35e69ea4aa52223cf98a
blockdreamers <blockdreamers@gmail.com>
2025-04-16 16:45:38 +0900

    vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_SUPERBASE_URL: string;
+    readonly VITE_SUPERBASE_ANON_KEY: string;
+    // 다른 VITE_ 변수들
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
